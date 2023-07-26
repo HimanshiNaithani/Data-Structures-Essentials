7b51c7f420f0d29a072b4dc11a4085133a55ea2d
HimanshiNaithani <100794236+HimanshiNaithani@users.noreply.github.com>
2023-07-26 11:42:49 -0700

    Add files via upload

---
+++ Arrays_Exercise/Search_Insert_Position.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+int binarySearch(int arr[],int n,int key){
+
+int s=0;
+int e=n-1;
+	while(s<=e)
+	{
+		int mid=(s+e)/2;
+		if(arr[mid]==key)
+		{
+			return mid;
+		}
+		else if(arr[mid]>key)
+		{
+			e=mid-1;
+		}
+		else
+		{
+			s=mid+1;
+		}
+		
+	}
+	return s;
+}
+int main()
+
+{
+	int arr[50];
+	int n;
+	cout<<"Enter n:"<<endl;
+	cin>>n;
+	cout<<"Enter the numbers:"<<endl;
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	cout<<"Enter the key you want to search:"<<endl;
+	int key;
+	cin>>key;
+	
+	int index=binarySearch(arr,n,key);
+    cout<<index;
+	
+}
