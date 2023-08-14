682905fa90477a358fc725a8e4b59ccaf55f1b5b
HimanshiNaithani <100794236+HimanshiNaithani@users.noreply.github.com>
2023-08-14 12:24:53 -0700

    Add files via upload

---
+++ Pattern Question/Pattern3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	int count=1;
+	cin>>n;
+	int i=1;
+	while(i<=n)
+	{
+		int j=1;
+		while(j<=n)
+		{
+			cout<<count<<" ";
+			count+=1;
+			j++;
+		}
+		cout<<endl;
+		i++;
+		  
+	 } 
+}
